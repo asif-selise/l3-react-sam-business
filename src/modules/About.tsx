8230f1b9edd1684e49d8b047c3fe3729f8f0fa2b
MD. ASIF BIN ABEDIN <asif.abedin@selisegroup.com>
2025-02-23 18:20:48 +0600

    refact: about page deleted

--- src/modules/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const About = () => {
-  return <div>About SAM Service7000</div>;
-};
-
-export default About;
--- src/modules/AppRouter/AppRouter.tsx
+++ src/modules/AppRouter/AppRouter.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
-import About from '../About';
 
 const AppRouter = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Dashboard />,
-    },
-    {
-      path: '/about',
-      element: <About />,
     },
   ]);
   return (
